b4e116d9c93e8d41781edc4bc2fe08d250eb270b
Betul <batool.beidas@gmail.com>
2022-07-02 16:36:43 +0300

    Fehlermeldung and custom calorie setting is added

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,39 +1,39 @@
 public class GeneticAlgorithm {
 
 
-public static final int populationSize = 25;
+    public static final int populationSize = 25;
     public static final int noOfEliteChromosomes = 1;
     public static final int tournamentSelectionSize = 24;
     private static final double mutationRate = 0.1;
 
-    public Population evolve(Population population){
+    public Population evolve(Population population) {
 
         return mutatePopulation(crossoverPopulation(population));
     }
 
-    private Population crossoverPopulation(Population population){
+    private Population crossoverPopulation(Population population) {
         Population crossoverPopulation = new Population(population.getChromosomes().length);
-        for(int i=0;i< noOfEliteChromosomes;i++){
+        for (int i = 0; i < noOfEliteChromosomes; i++) {
             crossoverPopulation.getChromosomes()[i] = population.getChromosomes()[i];
 
         }
-     for(int i=noOfEliteChromosomes;i<population.getChromosomes().length;i++){
-         Chromosome chromosome1 = selecetTournamentPopulation(population).getChromosomes()[0];
-         Chromosome chromosome2 = selecetTournamentPopulation(population).getChromosomes()[0];
-         crossoverPopulation.getChromosomes()[i] = crossoverChromosome(chromosome1,chromosome2);
+        for (int i = noOfEliteChromosomes; i < population.getChromosomes().length; i++) {
+            Chromosome chromosome1 = selectTournamentPopulation(population).getChromosomes()[0];
+            Chromosome chromosome2 = selectTournamentPopulation(population).getChromosomes()[0];
+            crossoverPopulation.getChromosomes()[i] = crossoverChromosome(chromosome1, chromosome2);
 
 
-     }
-         return crossoverPopulation;
+        }
+        return crossoverPopulation;
     }
 
-    private Population mutatePopulation(Population population){
+    private Population mutatePopulation(Population population) {
         Population mutatePopulation = new Population(population.getChromosomes().length);
-        for(int i=0;i< noOfEliteChromosomes;i++){
+        for (int i = 0; i < noOfEliteChromosomes; i++) {
             mutatePopulation.getChromosomes()[i] = population.getChromosomes()[i];
 
         }
-        for(int i=noOfEliteChromosomes;i<population.getChromosomes().length;i++){
+        for (int i = noOfEliteChromosomes; i < population.getChromosomes().length; i++) {
             mutatePopulation.getChromosomes()[i] = mutateChromosome(population.getChromosomes()[i]);
 
 
@@ -44,37 +44,36 @@ public static final int populationSize = 25;
     }
 
 
-
-    private Chromosome crossoverChromosome(Chromosome chromosome1,Chromosome chromosome2){
+    private Chromosome crossoverChromosome(Chromosome chromosome1, Chromosome chromosome2) {
         // random genes selection from parent chromosomes
         Chromosome crossoverChromosome = new Chromosome();
 
-        for(int i=0; i<chromosome1.getGenes().length;i++){
-            if(Math.random()>0.5) crossoverChromosome.getGenes()[i]= chromosome1.getGenes()[i];
-            else crossoverChromosome.getGenes()[i]= chromosome2.getGenes()[i];
+        for (int i = 0; i < chromosome1.getGenes().length; i++) {
+            if (Math.random() > 0.5) crossoverChromosome.getGenes()[i] = chromosome1.getGenes()[i];
+            else crossoverChromosome.getGenes()[i] = chromosome2.getGenes()[i];
         }
         return crossoverChromosome;
     }
 
-    private Chromosome mutateChromosome (Chromosome chromosome){
+    private Chromosome mutateChromosome(Chromosome chromosome) {
 
         Chromosome mutateChromosome = new Chromosome();
-        for(int i=0;i<chromosome.getGenes().length;i++){
-            if(Math.random()<mutationRate){
-                if(Math.random()<0.5) mutateChromosome.getGenes()[i]=1;
-                else mutateChromosome.getGenes()[i]=0;
+        for (int i = 0; i < chromosome.getGenes().length; i++) {
+            if (Math.random() < mutationRate) {
+                if (Math.random() < 0.5) mutateChromosome.getGenes()[i] = 1;
+                else mutateChromosome.getGenes()[i] = 0;
 
-            }else mutateChromosome.getGenes()[i]=chromosome.getGenes()[i];
-          }
+            } else mutateChromosome.getGenes()[i] = chromosome.getGenes()[i];
+        }
         return mutateChromosome;
     }
 
-    private Population selecetTournamentPopulation(Population population){
+    private Population selectTournamentPopulation(Population population) {
         Population tournamentPopulation = new Population(tournamentSelectionSize);
 
-        for(int i=0;i<tournamentSelectionSize;i++){
+        for (int i = 0; i < tournamentSelectionSize; i++) {
 
-            tournamentPopulation.getChromosomes()[i] = population.getChromosomes()[(int)(Math.random()*population.getChromosomes().
+            tournamentPopulation.getChromosomes()[i] = population.getChromosomes()[(int) (Math.random() * population.getChromosomes().
                     length)];
         }
         tournamentPopulation.sortChromosomesByFitness();
--- src/Main.java
+++ src/Main.java
@@ -8,87 +8,93 @@ import java.util.Scanner;
 public class Main {
 
     public static int maxCalorieIntake;
+
     public static void main(String[] args) throws IOException {
         Reader3ammo reader = new Reader3ammo();
         reader.CSVReaderMethod();
-
-
-        boolean pregnancySit=false;
-
-
         Scanner s = new Scanner(System.in);
+        System.out.println("Welcome!");
+        System.out.println("Disclaimer: this program is suitable for people between the age of 18 and 65 and don't have health issues," +
+                " and the recommendations do not compensate for medical consultation");
         System.out.println("Welcome! Please enter your age.");
+
         int age = s.nextInt();
+        while (age > 65 || age < 18) {
+            System.out.println("This program is suitable for users between the age of 18 and 65\n");
+            System.out.println("Please enter your age:\n");
+            age = s.nextInt();
+        }
 
         System.out.println("Please select your gender: press 1 (for Female)/or 2 (for Male)");
         int gender = s.nextInt();
-
-        if(gender==1){
-
-            System.out.println("Are you pregnant? (y/n)");
-            s.nextLine();
-            String pregnancy = s.nextLine();
-            if(pregnancy=="y"){
-                pregnancySit=true;
-
-
-
-            }else if(pregnancy=="n"){
-
-
-
-            }
-
-        }else if(gender==2){
-
+        while (gender != 1 && gender != 2) {
+            System.out.println("Please enter a valid value: choose 1 (for Female)/or 2 (for Male)");
+            gender = s.nextInt();
         }
+        s.nextLine();
 
-        System.out.println("Please enter your weight in kg");
-        int weight = s.nextInt();
-
-        calculateMaxCalorie(age,gender,pregnancySit,weight);
+        System.out.println("Are you looking to lose weight? y/n");
+        boolean loseWeight = false;
+        if (s.nextLine().equals("y")) {
+            loseWeight = true;
+        }
+        calculateMaxCalorie(gender, loseWeight);
 
         Population population = new Population(GeneticAlgorithm.populationSize).initializePopulation();
         GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm();
         System.out.println("------------------------");
-        System.out.println("Generation #0"+"| Fittest chromosome fitness:"+population.getChromosomes()[0].getFitness());
+        System.out.println("Generation #0" + "| Fittest chromosome fitness:" + population.getChromosomes()[0].getFitness());
         printPopulation(population);
 
         int generationNumber = 0;
-        while (generationNumber<50){
+        while (generationNumber < 50) {
             generationNumber++;
             System.out.println("\n ---------------------------");
             population = geneticAlgorithm.evolve(population);
             population.sortChromosomesByFitness();
-            System.out.println("Generation #"+ generationNumber+ " | Fittest chromosome fitness:"+population.getChromosomes()[0].getFitness());
+            System.out.println("Generation #" + generationNumber + " | Fittest chromosome fitness:" + population.getChromosomes()[0].getFitness()
+                    +" (total grams of protein )");
             printPopulation(population);
-
         }
 
 
     }
 
-    public static void calculateMaxCalorie(int age, int gender, boolean pregnancy, int weight){
-         maxCalorieIntake=2400;
+    public static void calculateMaxCalorie(int gender, boolean loseWeight) {
+        if (loseWeight) {
+            if (gender == 1) {
+                maxCalorieIntake = 1500;
+            } else {
+                maxCalorieIntake = 2000;
+            }
+        } else {
+            if (gender == 1) {
+                maxCalorieIntake = 2000;
+            } else {
+                maxCalorieIntake = 2500;
+            }
+        }
     }
 
     public static void printPopulation(Population population) throws IOException {
         printIngredients(population.getChromosomes()[0]);
         System.out.println("------------------------");
-        for(int i=0;i<population.getChromosomes().length;i++){
-
-            System.out.println("Chromosome #" +i+":"+ Arrays.toString(population.getChromosomes()[i].getGenes())+
-             "| Fitness" + population.getChromosomes()[i].getFitness());
+        for (int i = 0; i < population.getChromosomes().length; i++) {
+            System.out.println("Chromosome #" + i + ":" + Arrays.toString(population.getChromosomes()[i].getGenes()) +
+                    "| Fitness" + population.getChromosomes()[i].getFitness());
         }
     }
 
     public static void printIngredients(Chromosome chromosome) throws IOException {
         Reader3ammo reader = new Reader3ammo();
         reader.CSVReaderMethod();
-        int [] genes = chromosome.getGenes();
-        for (int i =0; i<chromosome.getGenesLength(); i++){
+        int[] genes = chromosome.getGenes();
+        for (int i = 0; i < chromosome.getGenesLength(); i++) {
             if (genes[i] == 1) {
-                System.out.println(reader.ingredients.get(i).getName() + ", protein: " + reader.ingredients.get(i).getProtein() + ", calories: " + reader.ingredients.get(i).getCalorie() + " ---- ");
+                System.out.println(reader.ingredients.get(i).getName() + " , GmWt: "+
+                        reader.ingredients.get(i).getGmWeight() +", GmDesc: "+ reader.ingredients.get(i).getGmDesc()
+                        + ", protein amount (gr): " + reader.ingredients.get(i).getProtein() + ", calories: "
+                        + reader.ingredients.get(i).getCalorie() + " . ");
             }
         }
 
